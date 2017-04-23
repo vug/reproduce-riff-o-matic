3ff4f37b81243c21fd0f5636b305caf7f89f41ea
Ugur Guney <ugurguney@gmail.com>
2017-04-23 19:02:48 -0400

    Study "car"/"cdr" getting first element and rest of lists.

--- studies/01_basics.lisp
+++ studies/01_basics.lisp
@@ -12,3 +12,10 @@
              't-var (null T)
              'empty-list (null ())
              'non-empty-list (null '(a b))))
+
+(print "3 car/cdr: first and rest of lists")
+; http://clhs.lisp.se/Body/f_car_c.htm
+(print (list 'list '(1 2 3)
+             'car (car '(1 2 3))
+             'cdr (cdr '(1 2 3))
+             'cdrcdrcdr (cdr (cdr (cdr '(1 2 3))))))
