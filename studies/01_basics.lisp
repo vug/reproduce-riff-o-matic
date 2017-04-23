6e2ae996ec081ad124b0d949f0ede251f8fa4e57
Ugur Guney <ugurguney@gmail.com>
2017-04-23 19:02:23 -0400

    Study "null" that checks list emptyness.

--- studies/01_basics.lisp
+++ studies/01_basics.lisp
@@ -3,3 +3,12 @@
 ; http://clhs.lisp.se/Body/s_setq.htm
 (setq a 1 b (+ 1 a) c (+ a b))
 (print (list 'a a 'b b 'c c))
+
+(print "2 null: check if NIL or empty list")
+; http://www.jtra.cz/stuff/lisp/sclr/null.html
+; http://clhs.lisp.se/Body/f_null.htm
+; Returns t if object is the empty list; otherwise, returns nil.
+(print (list 'nil-var (null NIL)
+             't-var (null T)
+             'empty-list (null ())
+             'non-empty-list (null '(a b))))
