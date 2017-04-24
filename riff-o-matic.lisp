97c3844cd4edb7342fcd134bb089cfad212451ce
Ugur Guney <ugurguney@gmail.com>
2017-04-24 01:20:36 -0400

    riff-o-matic into stdout or a file. +
    Separate setting of OUTFILE from the rest of the riff-o-matic logic so that we can set it separately and be ablo to write into different streams.

--- riff-o-matic.lisp
+++ riff-o-matic.lisp
@@ -1,13 +1,25 @@
+(defun riff-o-matic-to-file (nnotes)
+ "Generate riff into asked file."
+  (let ()
+    (princ "output file? ")
+    (setq OUTFILE (outfile (read)))
+    (riff-o-matic nnotes)
+    (close OUTFILE)))
+
+(defun riff-o-matic-to-stdout (nnotes)
+  "Generate riff into stdout."
+  (let ()
+    (setq OUTFILE *standard-output*)
+    (riff-o-matic nnotes)))
+
 (defun riff-o-matic (nnotes)
-  (prog ((count 0))
-        (princ "output file? ")
-        (setq OUTFILE (outfile (read)))
+  "Generate a riff of nnotes notes and send them to OUTFILE."
+  (let ((count 0))
         (setq START 0)
         (setq PREVPITCH 4.07)
         (setq WORKRHYTHS RHYTHMS)
         (loop repeat nnotes do
-          (generator (eval (getel INFLECTIONS))))
-       	(close OUTFILE)))
+          (generator (eval (getel INFLECTIONS))))))
 
 (defun generator (inflist)
   (map 'nil 'eval inflist))
